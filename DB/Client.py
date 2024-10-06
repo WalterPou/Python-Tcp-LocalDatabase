c5c557d8a353aa178d61667d67c32163d1ab6d2f
Eiqram <151066160+EiqramNas@users.noreply.github.com>
2024-10-06 18:24:12 +0800

    Update Client.py

--- DB/Client.py
+++ DB/Client.py
@@ -9,9 +9,11 @@ client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((h,p))
 
 def order(client):
-    while True:                                                                                   msg = client.recv(1024).decode()
+    while True:                                                                                   
+        msg = client.recv(1024).decode()
         if not msg:
-            break                                                                                 print(f'> ', end=' ')
+            break                                                                                 
+        print(f'> ', end=' ')
         for char in msg:
             print(char, end='', flush=True)
             time.sleep(0.01)
